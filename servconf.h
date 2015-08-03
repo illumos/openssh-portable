3b18401cad6c387a7e932b95e71ecbda918705ed
Alex Wilson <alex.wilson@joyent.com>
2015-08-03 16:27:44 -0700

    PubKeyPlugin support
    This adds the PubKeyPlugin directive and associated code from
    SunSSH, allowing an in-process shared library to be called
    into to check public keys for authentication.

--- servconf.h
+++ servconf.h
@@ -242,6 +242,7 @@ typedef struct {
 
 	int	fingerprint_hash;
 	int	expose_userauth_info;
+	char	*pubkey_plugin;
 	u_int64_t timing_secret;
 	char   *sk_provider;
 	int	required_rsa_size;	/* minimum size of RSA keys */
