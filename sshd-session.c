d2d99843d0c50273d87f45da3e3ee5254a97461d
Andy Fiddaman <illumos@fiddaman.net>
2024-07-02 09:18:06 +0000

    Restore tcpwrappers libwrap support

--- sshd-session.c
+++ sshd-session.c
@@ -70,6 +70,11 @@
 #include <prot.h>
 #endif
  
+#ifdef LIBWRAP
+#include <tcpd.h>
+#include <syslog.h>
+#endif /* LIBWRAP */
+
 #include "xmalloc.h"
 #include "ssh.h"
 #include "ssh2.h"
@@ -1268,6 +1273,25 @@ main(int ac, char **av)
 #ifdef SSH_AUDIT_EVENTS
 	audit_connection_from(ssh, remote_ip, remote_port);
 #endif
+
+#ifdef LIBWRAP
+	int allow_severity = options.log_facility|LOG_INFO;
+	int deny_severity = options.log_facility|LOG_WARNING;
+	/* Check whether logins are denied from this host. */
+	if (ssh_packet_connection_is_on_socket(ssh)) {
+		struct request_info req;
+
+		request_init(&req, RQ_DAEMON, __progname, RQ_FILE, sock_in, 0);
+		fromhost(&req);
+
+		if (!hosts_access(&req)) {
+			debug("Connection refused by tcp wrapper");
+			refuse(&req);
+			/* NOTREACHED */
+			fatal("libwrap refuse returns");
+		}
+	}
+#endif /* LIBWRAP */
 
 	rdomain = ssh_packet_rdomain_in(ssh);
 
